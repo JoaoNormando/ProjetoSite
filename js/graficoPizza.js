97fdd406f71db30e468240a7a8ece09459a783ac
JoaoNormando <joaonormando50@gmail.com>
2017-04-12 15:19:49 -0300

    Inserção de grafico

---
+++ js/graficoPizza.js
@@ -0,0 +1,52 @@
+Highcharts.chart('container', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Grafico da área de biomas brasileiro'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Amazônia',
+            y: 49.29
+        }, {
+            name: 'Cerrado',
+            y: 23.92,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Mata Atlântica',
+            y: 13.04
+        }, {
+            name: 'Caatinga',
+            y: 9.92
+        }, {
+            name: 'Pampa',
+            y: 2.07
+        }, {
+            name: 'Pantanal',
+            y: 1.76
+        }]
+    }]
+});
